da7c06e2842419f313a96191094bdfd83ed6899d
ThatGravyBoat <thatgravyboat@gmail.com>
2021-05-25 19:23:23 -0230

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let points = 0;
 ComfyJS.onCommand = ( user, command, message, flags, extra) => {
     if (command === params.get( "user" ) + "addpoint" && (flags.mod || flags.broadcaster)) {
         points++;
-        document.getElementById("points").innerText = `Points: ${points}`
+        document.getElementById("points").innerText = `${points}`
     }
     else if (command === params.get( "user" ) + "setpoint" && (flags.mod || flags.broadcaster)) {
         points = Number(message);
-        document.getElementById("points").innerText = `Points: ${points}`
+        document.getElementById("points").innerText = `${points}`
     }
 }
 
